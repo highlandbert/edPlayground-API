b490c53914eb95a8d81aae579cdcdaf2c0756d99
highlandbert <roberto.sl.90@gmail.com>
2018-03-22 01:22:21 +0100

    cors

--- src/server.js
+++ src/server.js
@@ -40,6 +40,12 @@ SupplementRoutes(app, router);
 LessonResultsRoutes(app, router);
 AuthRoutes(app, router);
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use('/api', router);
 
 app.listen(port);
