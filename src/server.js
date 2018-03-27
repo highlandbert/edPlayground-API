5335b2ae96404546166c9822277155d032ac90bc
highlandbert <roberto.sl.90@gmail.com>
2018-03-28 01:34:18 +0200

    .

--- src/server.js
+++ src/server.js
@@ -42,7 +42,7 @@ AuthRoutes(app, router);
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, x-access-token, X-Requested-With, Content-Type, Accept');
   next();
 });
 
